cc5c531fb63ecc00a021251beb962992bf208999
Mohammed <moh_wadie@yahoo.com>
2024-09-28 15:29:29 +0300

    EndOfSection3-2

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,12 +1,8 @@
 import { Component, EventEmitter, Output, Input, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { type NewTaskData } from '../task/task.model';
 import { TasksServices } from '../tasks.services';
 
 @Component({
   selector: 'app-new-task',
-  standalone: true,
-  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
 })
--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,13 +1,9 @@
 import { Component, Input, inject } from '@angular/core';
 import { type Task } from './task.model';
-import { CardComponent } from '../../shared/card/card.component';
-import { DatePipe } from '@angular/common';
 import { TasksServices } from '../tasks.services';
 
 @Component({
   selector: 'app-task',
-  standalone: true,
-  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
